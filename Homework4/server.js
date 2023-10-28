1ca579fdcffc4985c10c9fe0b5b2442c73bbcd92
Michael Dacanay <michaeldacanayusa@gmail.com>
2023-10-27 20:57:03 -0400

    Use port 80 for express app and add Dockerfile

--- Homework4/server.js
+++ Homework4/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Designate the public folder as serving static resources
 app.use(express.static('static'));
